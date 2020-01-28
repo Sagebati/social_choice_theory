20fc4d2e76fe76cd4ef3e69345dc02827bc133ac
Samuel Batissou <samuelbatissou@gmail.com>
2020-01-28 13:27:33 +0100

    Added one stage votation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::hash::Hash;
 use std::ops::Deref;
+use itertools::Itertools;
 
 pub trait Condorcet<T> {
     fn condorcet_winner(&self) -> Option<&T>;
+}
+
+pub trait OneStage<T> {
+    fn one_stage(&self) -> Option<&T>;
 }
 
 #[derive(Clone, Debug, Eq, PartialOrd, PartialEq)]
@@ -64,6 +69,31 @@ impl<T: Eq + Hash + Clone> Condorcet<T> for Vote<T> {
     }
 }
 
+impl<T: Eq + Hash + Clone> OneStage<T> for Vote<T> {
+    fn one_stage(&self) -> Option<&T> {
+        let mut scores: HashMap<&T, usize> = HashMap::new();
+        for (n, candidate) in self.ballots.iter()
+            .map(|(n, ballot)| (n, &ballot[0]))
+        {
+            scores.entry(candidate)
+                .and_modify(move |x| *x += *n)
+                .or_insert(0);
+        }
+        let mut winner = None;
+        for (candidate, score) in scores.iter().sorted_by_key(|s| s.1) {
+            if let Some((candidate_old, score_before)) = winner {
+                return if score_before == score {
+                    None
+                } else {
+                    Some(candidate_old)
+                }
+            }
+            winner = Some((candidate, score));
+        }
+        unreachable!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{Condorcet, PreOrder, Vote};
@@ -73,7 +103,7 @@ mod tests {
     fn condorcet_1() {
         assert_eq!(
             Vote {
-                candidates: hset!("a", "b", "c",),
+                candidates: hset!("a", "b", "c", ),
                 ballots: vec![
                     (35, PreOrder(vec!["a", "b", "c"])),
                     (25, PreOrder(vec!["b", "c", "a"])),
@@ -90,7 +120,7 @@ mod tests {
     fn condorcet_2() {
         assert_eq!(
             Vote {
-                candidates: hset!("a", "b", "c","d"),
+                candidates: hset!("a", "b", "c", "d"),
                 ballots: vec![
                     (42, PreOrder(vec!["a", "b", "c", "d"])),
                     (26, PreOrder(vec!["b", "c", "d", "a"])),
